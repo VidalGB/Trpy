c6a5b581c3ddc1718e136da45d0d5c4f75766e18
Manuel García <manuelgarcia0660@gmail.com>
2022-10-12 21:28:55 -0300

    Fixing the file format

--- main.py
+++ main.py
@@ -1,85 +1,150 @@
-# Python v3.9.2 more information and dependencies, read requirements.txt
-# Syntax camelCase蟒
+''' Python v3.9.2 more information and dependencies, read requirements.txt
+    Syntax camelCase '''
+
 # Imports
 import argparse
 from locale import getdefaultlocale
-from src import translators
+from src import translators as tr
 import sys
 
 # Main function
 def main():
     langSystem = systemLanguage()
 
-# Supported languages
-    language = {'zh': 'chinese', 'en': 'english', 'de': 'german', 'it': 'italian', 'pl': 'polish', 'pt': 'portuguese', 'ru': 'russian', 'es': 'spanish', 'sv': 'swedish', 'fr': 'french', None: None}
+    # Supported languages
+    language = {
+        "zh": "chinese",
+        "en": "english",
+        "de": "german",
+        "it": "italian",
+        "pl": "polish",
+        "pt": "portuguese",
+        "ru": "russian",
+        "es": "spanish",
+        "sv": "swedish",
+        "fr": "french",
+        None: None,
+    }
 
-# Defining name, use and definition
-    parser = argparse.ArgumentParser(prog = 'Trpy', formatter_class = argparse.RawDescriptionHelpFormatter, description = "Trpy is a command line translator, intended to be as practical and fast as possible.\nThe supported languages are: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n\nThis Translator was developed by @VidalGB.\nYou can find the source code at: https://github.com/VidalGB/Trpy")
+    # Defining name, use and definition
+    parser = argparse.ArgumentParser(
+        prog="Trpy",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Trpy is a command line translator, intended to be as practical and fast as possible.\nThe supported languages are: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n\nThis Translator was developed by @VidalGB.\nYou can find the source code at: https://github.com/VidalGB/Trpy",
+    )
 
-# Version argument
-    parser.add_argument('-v', '--version', action = 'version', version = '%(prog)s 1.0', help = "show program's version number and exit.")
+    # Version argument
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s 1.0",
+        help="show program's version number and exit.",
+    )
 
-# Of language argument
-    parser.add_argument("-o","--of", type = str, default = None, help = "Language of the text to be translated (it can be abbreviated, example: 'en' or 'english'), by default it is automatic, but it is not recommended.")
+    # Of language argument
+    parser.add_argument(
+        "-o",
+        "--of",
+        type=str,
+        default=None,
+        help="Language of the text to be translated (it can be abbreviated, example: 'en' or 'english'), by default it is automatic, but it is not recommended.",
+    )
 
-# To language argument
-    parser.add_argument("-t", "--to", type = str, default = f"{langSystem}", help = "Language to translate the text (it can be abbreviated, example: 'en' or 'english'), by default it is the language of your operating system.")
+    # To language argument
+    parser.add_argument(
+        "-t",
+        "--to",
+        type=str,
+        default=f"{langSystem}",
+        help="Language to translate the text (it can be abbreviated, example: 'en' or 'english'), by default it is the language of your operating system.",
+    )
 
-# Message argument
-    parser.add_argument("-m", "--message", type = str, required = True, help = 'Text to translate, in quotes "Example".')
+    # Message argument
+    parser.add_argument(
+        "-m",
+        "--message",
+        type=str,
+        required=True,
+        help='Text to translate, in quotes "Example".',
+    )
 
-# DeepL translator argument (free)
-    parser.add_argument("-d", action = 'store_true', help = 'Use the DeepL translator with auth key in version Free.')
+    # DeepL translator argument (free)
+    parser.add_argument(
+        "-d",
+        action="store_true",
+        help="Use the DeepL translator with auth key in version Free.",
+    )
 
-# DeepL translator argument (pro)
-    parser.add_argument("-dp", action = 'store_true', help = 'Use the DeepL translator with auth key in version Pro.')
+    # DeepL translator argument (pro)
+    parser.add_argument(
+        "-dp",
+        action="store_true",
+        help="Use the DeepL translator with auth key in version Pro.",
+    )
 
-# Microsoft translator argument
-    parser.add_argument("-mi", action = 'store_true', help = 'Use the Microsoft translator.')
+    # Microsoft translator argument
+    parser.add_argument(
+        "-mi", action="store_true", help="Use the Microsoft translator."
+    )
 
-# My memory translator argument
-    parser.add_argument("-me", action = 'store_true', help = 'Use the MyMemory translator.')
+    # My memory translator argument
+    parser.add_argument("-me", action="store_true", help="Use the MyMemory translator.")
 
-# Google translator argument
-    parser.add_argument("-g", action = 'store_true', help = 'Use the Google translator.')
+    # Google translator argument
+    parser.add_argument("-g", action="store_true", help="Use the Google translator.")
     args = parser.parse_args()
+    print(args)
 
-# Controlling language and referring translators
-    controlLanguage(args, language)
+    controlLanguage(args, language) # Controlling language and referring translators
 
-# Control language
+
 def controlLanguage(args, language):
+    '''Control language suport'''
+
     of = [True for key, value in language.items() if args.of == key or args.of == value]
     to = [True for key, value in language.items() if args.to == key or args.to == value]
     if to == [True] and of == [True]:
 
-# Referring translators
-        if args.d: translators.deeplTranslate(args)
-        elif args.dp: translators.deeplTranslate(args)
-        elif args.g: translators.googleTranslate(args)
-        elif args.mi: translators.microsoftTranslate(args)
-        elif args.me: translators.memoryTranslate(args)
-        else: translators.translate(args)
+        # Referring translators
+        if args.d:
+            tr.deeplTranslate(args)
+        elif args.dp:
+            tr.deeplTranslate(args)
+        elif args.g:
+            tr.googleTranslate(args)
+        elif args.mi:
+            tr.microsoftTranslate(args)
+        elif args.me:
+            tr.memoryTranslate(args)
+        else:
+            tr.translate(args)
 
     else:
         if to != [True]:
-            sys.stdout.write(f'The language "{args.to}" is not supported, please use one of the following languages: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n')
+            sys.stdout.write(
+                f'The language "{args.to}" is not supported, please use one of the following languages: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n'
+            )
         if of != [True]:
-            sys.stdout.write(f'The language "{args.of}" is not supported, please use one of the following languages: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n')
+            sys.stdout.write(
+                f'The language "{args.of}" is not supported, please use one of the following languages: Chinese, English, German, Italian, Polish, Portuguese, Russian, Spanish, Swedish, French.\n'
+            )
 
-# Function to detect the system language
+
 def systemLanguage():
+    '''Function to detect the system language'''
+    
     try:
         locate = getdefaultlocale()
         locate = locate[0]
         locate = locate.split("_")
-        locate = locate[0]
-        return locate
+        return locate[0]
 
-# Error
-    except:
-        sys.stdout.write("Unexpected error when trying to get your language from the operating system (to avoid this error put -t or --to 'your language'), please report this error.\n")
+    except: # Error
+        sys.stdout.write(
+            "Unexpected error when trying to get your language from the operating system (to avoid this error put -t or --to 'your language'), please report this error.\n"
+        )
 
 # Check script main
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/translators.py
+++ src/translators.py
@@ -1,98 +1,131 @@
 from translate import Translator
 from requests.exceptions import ConnectionError
-from deep_translator import GoogleTranslator, MicrosoftTranslator, MyMemoryTranslator, DeeplTranslator
+from deep_translator import (
+    GoogleTranslator,
+    MicrosoftTranslator,
+    MyMemoryTranslator,
+    DeeplTranslator,
+)
 import sys
 
-# DeepL translator function
-def deeplTranslate(args):
+def deeplTranslate(args: tuple):
+    '''DeepL translator function'''
+    
     try:
-
-# DeepL Pro
-        if args.dp:
-            authKey = str(input('Enter the DeepL auth key to continue: '))
+        if args.dp: # DeepL Pro
+            authKey = str(input("Enter the DeepL auth key to continue: "))
             if args.of == None:
-                translation = DeeplTranslator(api_key = authKey, source = "auto", target = args.to, use_free_api = False).translate(args.message)
+                translation = DeeplTranslator(
+                    api_key=authKey, source="auto", target=args.to, use_free_api=False
+                ).translate(args.message)
             else:
-                translation = DeeplTranslator(api_key = authKey, source = args.of, target = args.to, use_free_api = False).translate(args.message)
-            sys.stdout.write(f'{translation}\n')
+                translation = DeeplTranslator(
+                    api_key=authKey, source=args.of, target=args.to, use_free_api=False
+                ).translate(args.message)
+            sys.stdout.write(f"{translation}\n")
 
-# DeepL Free
-        else:
-            authKey = str(input('Enter the DeepL auth key to continue: '))
+        else: # DeepL Free
+            authKey = str(input("Enter the DeepL auth key to continue: "))
             if args.of == None:
-                translation = DeeplTranslator(api_key = authKey, source = "auto", target = args.to, use_free_api = True).translate(args.message)
+                translation = DeeplTranslator(
+                    api_key=authKey, source="auto", target=args.to, use_free_api=True
+                ).translate(args.message)
             else:
-                translation = DeeplTranslator(api_key = authKey, source = args.of, target = args.to, use_free_api = True).translate(args.message)
-            sys.stdout.write(f'{translation}\n')
+                translation = DeeplTranslator(
+                    api_key=authKey, source=args.of, target=args.to, use_free_api=True
+                ).translate(args.message)
+            sys.stdout.write(f"{translation}\n")
 
-# Connection Error
-    except ConnectionError:
-        sys.stdout.write('Connection error, please provide your connection and try again.\n')
+    except ConnectionError: # Connection Error
+        sys.stdout.write(
+            "Connection error, please provide your connection and try again.\n"
+        )
     except deep_translator.exceptions.AuthorizationException as key:
-        sys.stdout.write(f'{key}\n')
+        sys.stdout.write(f"{key}\n")
     except:
         sys.stdout.write("Unexpected error, please report this error.\n")
 
-# Google translator function
-def googleTranslate(args):
+
+def googleTranslate(args: tuple):
+    '''Google translator function'''
+    
     try:
         if args.of == None:
-            translation = GoogleTranslator(source = 'auto', target = args.to).translate(text = args.message)
+            translation = GoogleTranslator(source="auto", target=args.to).translate(
+                text=args.message
+            )
         else:
-            translation = GoogleTranslator(source = args.of, target = args.to).translate(text = args.message)
-        sys.stdout.write(f'{translation}\n')
+            translation = GoogleTranslator(source=args.of, target=args.to).translate(
+                text=args.message
+            )
+        sys.stdout.write(f"{translation}\n")
 
-# Connection Error
-    except ConnectionError:
-        sys.stdout.write('Connection error, please provide your connection and try again.\n')
+    except ConnectionError: # Connection Error
+        sys.stdout.write(
+            "Connection error, please provide your connection and try again.\n"
+        )
     except:
         sys.stdout.write("Unexpected error, please report this error.\n")
 
-# My Memory translator function
-def memoryTranslate(args):
+
+def memoryTranslate(args: tuple):
+    '''My Memory translator function'''
     try:
         if args.of == None:
-            sys.stdout.write("MyMemory translator does not have automatic language detection, please add a language with -o or --of (it can be shortened, example: 'en' or 'english'\n")
+            sys.stdout.write(
+                "MyMemory translator does not have automatic language detection, please add a language with -o or --of (it can be shortened, example: 'en' or 'english'\n"
+            )
         else:
-            translation = MyMemoryTranslator(source = args.of, target = args.to).translate(args.message)
-            sys.stdout.write(f'{translation}\n')
+            translation = MyMemoryTranslator(source=args.of, target=args.to).translate(
+                args.message
+            )
+            sys.stdout.write(f"{translation}\n")
 
-# Connection Error
-    except ConnectionError:
-        sys.stdout.write('Connection error, please provide your connection and try again.\n')
+    except ConnectionError: # Connection Error
+        sys.stdout.write(
+            "Connection error, please provide your connection and try again.\n"
+        )
     except:
         sys.stdout.write("Unexpected error, please report this error.\n")
 
-# Microsoft translator function
-def microsoftTranslate(args):
-    try:
-        authKey = str(input('Enter the DeepL auth key to continue: '))
-        if args.of == None:
-            translation = MicrosoftTranslator(api_key = authKey, source = "auto", target = args.to).translate(text = args.message)
-        else:
-            translation = MicrosoftTranslator(api_key = authKey, source = args.of, target = args.to).translate(text = args.message)
-        sys.stdout.write(f'{translation}\n')
 
-# Connection Error
-    except ConnectionError:
-        sys.stdout.write('Connection error, please provide your connection and try again.\n')
+def microsoftTranslate(args: tuple):
+    '''Microsoft translator function'''
+    try:
+        authKey = str(input("Enter the DeepL auth key to continue: "))
+        if args.of == None:
+            translation = MicrosoftTranslator(
+                api_key=authKey, source="auto", target=args.to
+            ).translate(text=args.message)
+        else:
+            translation = MicrosoftTranslator(
+                api_key=authKey, source=args.of, target=args.to
+            ).translate(text=args.message)
+        sys.stdout.write(f"{translation}\n")
+
+    except ConnectionError: # Connection Error
+        sys.stdout.write(
+            "Connection error, please provide your connection and try again.\n"
+        )
     except deep_translator.exceptions.MicrosoftAPIerror as key:
-        sys.stdout.write(f'Unauthorized access with this api key\n')
+        sys.stdout.write(f"Unauthorized access with this api key\n")
     except:
         sys.stdout.write("Unexpected error, please report this error.\n")
 
 
-# Translator function
-def translate(args):
+def translate(args: tuple):
+    '''Translator function'''
+    
     try:
-        translator = Translator(to_lang = args.to)
+        translator = Translator(to_lang=args.to)
         if not args.of == None:
-            translator = Translator(from_lang = args.of,to_lang = args.to)
+            translator = Translator(from_lang=args.of, to_lang=args.to)
         translation = translator.translate(args.message)
-        sys.stdout.write(f'{translation}\n')
+        sys.stdout.write(f"{translation}\n")
 
-# Connection Error
-    except ConnectionError:
-        sys.stdout.write('Connection error, please provide your connection and try again.\n')
+    except ConnectionError: # Connection Error
+        sys.stdout.write(
+            "Connection error, please provide your connection and try again.\n"
+        )
     except:
         sys.stdout.write("Unexpected error, please report this error.\n")
